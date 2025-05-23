8258bfc4c2ffc284098dffc64819c80b00c790d4
Muzaffarmirzo Choriyev <muzaffarmirzo.choriyev@ucell.uz>
2025-05-23 19:57:41 +0500

    🔼Updated: changes

--- core/models/patients.py
+++ core/models/patients.py
@@ -46,7 +46,7 @@ class PatientModel(models.Model):
             except:
                 mid_name = ''
 
-            return f"{self.l_name} {self.f_name} {mid_name}"
+            return f"{mid_name} {self.f_name} {self.l_name}"
         except:
             return ""
 
